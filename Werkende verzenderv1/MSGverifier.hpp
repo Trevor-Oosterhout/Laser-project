00e8d8337c04415c8f9ea4830d6cd2c75264e862
Jvandekamer <60784651+Jvandekamer@users.noreply.github.com>
2020-10-31 12:51:24 +0100

    1e werkende verzender toegevoegd
    De testcode van de eerste succesvolle IR test (zender)

---
+++ Werkende verzenderv1/MSGverifier.hpp
@@ -0,0 +1,72 @@
+#ifndef MSGVERIFIER_HPP
+#define MSGVERIFIER_HPP
+
+#include "hwlib.hpp"
+#include "rtos.hpp"
+#include <stdint.h>
+
+class MSGverifier: public rtos::task<>{
+private:
+    rtos::channel <uint16_t, 10> verificationchannel;
+    rtos::timer doublemessagetimer;
+    
+    void main(){
+        uint16_t lastmessage, receivedmessage;
+        int playerID;
+        bool XOR;
+        for(;;){
+            auto event = wait(verificationchannel + doublemessagetimer);
+            if(event == verificationchannel){
+                receivedmessage = verificationchannel.read();
+                XOR = checkbit(receivedmessage);
+                if(XOR){
+                    playerID = (receivedmessage & 0x7c00);
+                    doublemessagetimer.set(42000);
+                    lastmessage = receivedmessage;
+                    if(lastmessage != receivedmessage){
+                        if(playerID > 0){
+                            hwlib::cout << "Dit bericht moet naar de Shootcontrol" << "\n";
+                        }
+                        else{
+                            hwlib::cout << "Dit bericht moet naar de registrationcontrol" << "\n";
+                        }
+                    }
+                }  
+            }
+            else{
+                lastmessage = 0x000f; // deze waarde kan niet worden geverifieerd, dus kan de message nooit gelijjk zijn aan deze waarde
+            }
+
+        }
+    }
+
+public:
+    MSGverifier():
+    task(1, "MSGverifier"),
+    verificationchannel(this,"verificationchannel"),
+    doublemessagetimer(this,"doublemessagetimer")
+    {}
+
+    void reportmessage(uint16_t msg){
+        verificationchannel.write(msg);
+    }
+
+    bool checkbit(uint16_t & msg){
+        uint16_t check, leftXOR, rightXOR;
+        bool XOR;
+        for(unsigned int i = 0; i < 5; i++){
+            check = msg >> (15-11+i) & 1;
+            leftXOR = msg >> (15-1+i) & 1;
+            rightXOR = msg >> (15-6+i) & 1;
+            XOR = (check == (leftXOR ^ rightXOR));
+            if(!XOR){
+                return false;
+            } 
+        }
+        return true;
+    }
+};
+
+
+
+#endif //MSGVERIFIER_HPP
---
+++ Werkende verzenderv1/SignalDetector.hpp
@@ -0,0 +1,61 @@
+#ifndef SIGNALDETECTOR_HPP
+#define SIGNALDETECTOR_HPP
+
+
+#include "hwlib.hpp"
+#include "MSGdecoder.hpp"
+#include "rtos.hpp"
+
+
+class SignalDetector : public rtos::task<>{
+private:
+    hwlib::pin_in & IRreceiver;
+    MSGdecoder & msgdecoder;
+    enum states {idle, count};
+    rtos::clock PollClock;
+    
+    void main(){
+        bool signal;
+        int n;
+        states state = idle;
+        for(;;){
+            switch (state){
+                case idle:
+                    wait(PollClock);
+                    IRreceiver.refresh();
+                    signal = IRreceiver.read();
+                    if(signal){
+                        state = idle;
+                    }
+                    else{
+                        n = 0;
+                        state = count;
+                    }
+                    break;
+                case count:
+                    wait(PollClock);
+                    IRreceiver.refresh();
+                    signal = IRreceiver.read();
+                    if(signal){
+                        msgdecoder.SignalReceived(n);
+                        state = idle;
+                    }
+                    else{
+                        n += 100;
+                        state = count;
+                    }
+                    break;
+            }
+        }
+    }  
+
+public:
+    SignalDetector(MSGdecoder & msgdecoder, hwlib::pin_in & IRreceiver):
+    task(1, "SignalDetector"),
+    IRreceiver(IRreceiver),
+    msgdecoder(msgdecoder),
+    PollClock(this,100,"PollClock")
+    {}
+};
+
+#endif // SIGNALDETECTOR_HPP
